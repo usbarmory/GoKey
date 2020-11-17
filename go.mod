c15907f66f447a7314faa40afdca5d08c68cfb12
Andrea Barisani <andrea@inversepath.com>
2020-11-17 21:55:32 +0100

    tidying

--- go.mod
+++ go.mod
@@ -4,14 +4,14 @@ go 1.15
 
 require (
 	github.com/f-secure-foundry/armoryctl v0.0.0-20201111102916-d2bb1b09dba7
-	github.com/f-secure-foundry/tamago v0.0.0-20201107175817-b08870c21469
-	github.com/gsora/fidati v0.0.0-20201111113948-b0d4c16708b9
+	github.com/f-secure-foundry/tamago v0.0.0-20201113223423-3d056ea6004a
+	github.com/gsora/fidati v0.0.0-20201117203042-f53e0d9ed50e
 	github.com/hsanjuan/go-nfctype4 v0.0.1
 	github.com/keybase/go-crypto v0.0.0-20200123153347-de78d2cb44f4
-	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897
-	golang.org/x/sys v0.0.0-20201112073958-5cba982894dd // indirect
+	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9
+	golang.org/x/sys v0.0.0-20201117170446-d9b008d0a637 // indirect
 	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
-	gvisor.dev/gvisor v0.0.0-20201112064609-ac62743e380c
+	gvisor.dev/gvisor v0.0.0-20201117183629-05d2a26f7a86
 )
 
 replace gvisor.dev/gvisor => github.com/f-secure-foundry/gvisor v0.0.0-20200812210008-801bb984d4b1
--- gokey_imx6.go
+++ gokey_imx6.go
@@ -13,6 +13,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 
 	"github.com/f-secure-foundry/GoKey/internal"
 	"github.com/f-secure-foundry/GoKey/internal/ccid"
@@ -22,8 +23,15 @@ import (
 
 	"github.com/f-secure-foundry/tamago/soc/imx6"
 	imxusb "github.com/f-secure-foundry/tamago/soc/imx6/usb"
+	"github.com/f-secure-foundry/tamago/soc/imx6/usb/ethernet"
 
 	_ "github.com/f-secure-foundry/tamago/board/f-secure/usbarmory/mark-two"
+)
+
+const (
+	hostMAC   = "1a:55:89:a2:69:42"
+	deviceMAC = "1a:55:89:a2:69:41"
+	IP        = "10.0.0.10"
 )
 
 func init() {
@@ -95,7 +103,7 @@ func main() {
 	}
 
 	if len(sshPublicKey) != 0 {
-		startNetworking(device, card, token)
+		configureNetworking(device, card, token)
 	}
 
 	imxusb.USB1.Init()
@@ -108,4 +116,59 @@ func main() {
 
 	// never returns
 	imxusb.USB1.Start(device)
+}
+
+func configureNetworking(device *imxusb.Device, card *icc.Interface, token *u2f.Token) {
+	// start basic networking
+	stack, link := gokey.StartNetworking(deviceMAC, IP)
+
+	if len(sshPublicKey) != 0 {
+		console := &gokey.Console{
+			Stack:         stack,
+			Interface:     1,
+			Address:       IP,
+			Port:          22,
+			AuthorizedKey: sshPublicKey,
+			PrivateKey:    sshPrivateKey,
+			Card:          card,
+			Token:         token,
+			Started:       make(chan bool),
+		}
+
+		// start SSH server for management console
+		err := console.Start()
+
+		if err != nil {
+			log.Printf("SSH server initialization error: %v", err)
+		}
+
+		// wait for ssh server to start before responding to USB requests
+		<-console.Started
+	}
+
+	hostAddress, err := net.ParseMAC(hostMAC)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	deviceAddress, err := net.ParseMAC(deviceMAC)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Configure Ethernet over USB endpoints
+	// (ECM protocol, only supported on Linux hosts).
+	eth := ethernet.NIC{
+		Host:   hostAddress,
+		Device: deviceAddress,
+		Link:   link,
+	}
+
+	err = eth.Init(device, 0)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
